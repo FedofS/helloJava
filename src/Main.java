1803f840c1701bca16f5b74beb3915e0b730b2f0
FedofS <7ff77ff@gmail.com>
2023-05-15 23:29:11 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+//        Hello World!
+
+        for (int i=0; i<=4; i++){
+            System.out.println(i);
+        }
+
+    }
+}
